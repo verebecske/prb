eb3f51e64882b65f241b159e1d9acc51784d0b0d
verebecske <julia.csuha@erlang-solutions.com>
2019-03-12 14:53:00 +0100

    :(

--- wombat_discovery/apps/wombat_discovery/src/wombat_discovery_app.erl
+++ wombat_discovery/apps/wombat_discovery/src/wombat_discovery_app.erl
@@ -15,6 +15,7 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+	io:format("Hello wombat_discovery_app start ~n"),
     wombat_discovery_sup:start_link().
 
 %%--------------------------------------------------------------------
--- wombat_discovery/apps/wombat_discovery/src/wombat_discovery_sup.erl
+++ wombat_discovery/apps/wombat_discovery/src/wombat_discovery_sup.erl
@@ -20,6 +20,7 @@
 %%====================================================================
 
 start_link() ->
+	io:format("Hello wombat_discovery_sup start_link ~n"),
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %%====================================================================
@@ -27,9 +28,44 @@ start_link() ->
 %%====================================================================
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
-init([]) ->
-    {ok, { {one_for_one, 0, 1}, []} }.
+%init([]) ->
+%    {ok, { {one_for_one, 0, 1}, []} }.
+
+%init(_Args) ->
+%    SupFlags = {one_for_one, 0, 1},
+%    ChildSpecs = [{automatic_connector,
+%                  ,
+%                   permanent, brutal_kill, worker,
+%                   [wombat_discovery]}],
+%	ChildSpecs = [{id, automatic_connector},
+%                  {mfargs,{automatic_connector,start_link,[]}},
+%                  {restart_type,permanent},
+%                  {shutdown,2000},
+%                  {child_type,worker}],
+%	ChildSpecs = %[{automatic_connector, {automatic_connector, start_link, []}, permanent, 5000, worker,[automatic_connector]}],
+%	 #{id => automatic_connector,
+%      start => {gen_server, start_link, [ automatic_connector]},
+%      restart => permanent,
+%      modules => [automatic_connector]},
+%    {ok, {SupFlags, []}}.   
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+init([]) ->
+	io:format("Hello wombat_discovery_sup init ~n"),
+    {ok, {{one_for_one, 5, 10},
+          [
+          child(automatic_connector, start_link, [])
+          ]}}.
+
+%%%=============================================================================
+%%% Internal functions
+%%%=============================================================================
+
+-spec child(Mod :: module(),
+            Fun :: atom(),
+            Args :: [term()]) -> supervisor:child_spec().
+child(Mod, Fun, Args) ->
+    {Mod, {Mod, Fun, Args}, permanent, 5000, worker, [Mod]}.
