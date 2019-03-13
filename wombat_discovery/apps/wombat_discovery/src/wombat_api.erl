fedc591c44f1ae76c3230399b8cc584dc9f88a2e
verebecske <julia.csuha@erlang-solutions.com>
2019-03-13 18:03:27 +0100

    ping work, gen_server:call don't

--- wombat_discovery/apps/wombat_discovery/src/wombat_api.erl
+++ wombat_discovery/apps/wombat_discovery/src/wombat_api.erl
@@ -7,12 +7,14 @@ discover_me(NodeName,Cookie) ->
 	erlang:set_cookie(NodeName,Cookie),
 	TargetCookie = erlang:get_cookie(),
 	TargetNode = erlang:node(),
-	Target = {wo_discover_dynamic_nodes, NodeName},
-	%Target = {me, 'tas@127.0.0.1'},
+	%Target = {wo_discover_dynamic_nodes, NodeName},
+	Target = {tas, 'tas@127.0.0.1'},
 	Request = {auto_discover_node, TargetNode, TargetCookie},
 	
+	io:format("Ping? ~p ~n", [net_adm:ping('tas@127.0.0.1')]),
+
 	try gen_server:call(Target, Request, ?TIMEOUT) of
-		Ans -> io:format("Hello api try ~p ~n",[Ans])
+		Ans -> io:format("Hello api try ~p ~n",[Ans]), Ans
 	catch
 		exit:Msg -> io:format("Hello api catch ~p ~n",[Msg]), no_connection;
 		exit:{{nodedown, _}, _} -> no_connection;
