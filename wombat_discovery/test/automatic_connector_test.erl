b6b182f7722e27b399d65781696faea6df027702
verebecske <julia.csuha@erlang-solutions.com>
2019-03-19 13:51:43 +0100

    start testing

---
+++ wombat_discovery/test/automatic_connector_test.erl
@@ -0,0 +1,9 @@
+-module(automatic_connector_test).
+-include_lib("eunit/include/eunit.hrl").
+
+-export([]).
+
+%When Connector is started, calls Wombat API to discover node
+connector_calls_wombat_api_test() ->
+	ok.
+	
